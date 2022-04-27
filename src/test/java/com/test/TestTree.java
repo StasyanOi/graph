05c75721370978687e1bcc8d6dca3f6155fc0a96
StasyanOi <iluvstrwrs@mail.ru>
2022-04-27 22:51:34 +0300

    added new test

--- src/test/java/com/test/TestTree.java
+++ src/test/java/com/test/TestTree.java
@@ -64,6 +64,39 @@ public class TestTree {
     }
 
     @Test
+    public void deleteMultipleValuesTest() {
+        var tree = getTestTree();
+
+        assertNotNull(tree.dfs(5));
+        assertNotNull(tree.dfs(-3.5));
+        assertNotNull(tree.dfs(-8));
+        assertNotNull(tree.dfs(0));
+        assertNotNull(tree.dfs(8));
+        var beforeDeleteString = tree.toString();
+
+        tree.delete(5);
+        tree.delete(-3.5);
+        tree.delete(-8);
+        tree.delete(0);
+        tree.delete(8);
+
+        assertNull(tree.dfs(5));
+        assertNull(tree.dfs(-3.5));
+        assertNull(tree.dfs(-8));
+        assertNull(tree.dfs(0));
+        assertNull(tree.dfs(8));
+        var afterDeleteString = tree.toString();
+
+        assertEquals(
+                beforeDeleteString
+                        .replace("4.0,5.0,", "4.0,")
+                        .replace("-3.5,", "")
+                        .replace("-8.0,", "")
+                        .replace("0.0,", "")
+                        .replace("8.0,", ""), afterDeleteString);
+    }
+
+    @Test
     public void rotateRightTest() {
         var tree = new Tree();
         tree.insert(0);
